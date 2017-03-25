9de9278aeb0c86c2ef3479da82ce2e005d274c21
Liazidi Amine <liazidiiamine@gmail.com>
2017-03-25 15:18:50 +0100

    debut mise en place du client

--- src/client/HTTPClient.java
+++ src/client/HTTPClient.java
@@ -1,5 +1,33 @@
 package client;
 
-public class HTTPClient {
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import com.fasterxml.jackson.*;
 
+public class HTTPClient {
+	
+	SocketAddress socketAd;
+	HTTPReader reader;
+	HTTPHeader header;
+	
+    public static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
+    public static final int BUFFER_SIZE = 1024;
+    
+    public static final String REQUEST_TASK = "GET Task HTTP/1.1\r\n "
+    											+ "Host: ***server address***\r\n "
+    											+ "\r\n";
+    
+    public HTTPClient(String host, int port) {
+		this.socketAd = new InetSocketAddress(host, port);
+	}
+    
+    public static String sendTaskRequest(String request, SocketAddress server){
+    	
+    }
+    
+    
+    
 }
